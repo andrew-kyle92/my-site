ca4ef4a56e4dd21aada54eee2db62692428a9d1c
andrew-kyle92 <andrew.kyle92@yahoo.com>
2021-05-06 14:44:27 -0700

    updated styling for the song list on about page.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -16,25 +16,6 @@ function tabOptions(evt, Option) {
     evt.currentTarget.className += " active";
 }
 
-// function playSong(evt) {
-//     let songs = document.getElementsByClassName("song");
-//     let songTitle = document.getElementsByClassName("song-title")[0];
-//     let songAlbum = evt.path[0].dataset['album']
-//     let songData = evt.path[0].dataset['song'];
-//     for(let i = 0; i < songs.length; i++){
-//         if(evt.path[0].innerText == songs[i].innerText && songAlbum == songs[i].getAttribute("data-album")){
-//             songs[i].classList.toggle("song-active");
-//             let songPath = `Music/${songAlbum}/${songData}`;
-//             console.log(songPath);
-//             songTitle.innerText = songs[i].innerText;
-//             document.getElementById("my-audio").setAttribute("src", songPath);
-//         }
-//         else{
-//             songs[i].className = "song";
-//         }
-//     }
-// }
-
 var songs = document.getElementsByClassName("song");
 for(let i = 0; i < songs.length; i++){
     songs[i].addEventListener("click", function(){
